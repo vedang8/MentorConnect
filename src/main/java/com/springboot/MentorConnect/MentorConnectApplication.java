8cc1444c6ee08dae9d3a2578a4e759404be9448d
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-05-28 20:56:41 +0530

    Mentor Connect is created

---
+++ src/main/java/com/springboot/MentorConnect/MentorConnectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.MentorConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/MentorConnect/MentorConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.MentorConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
